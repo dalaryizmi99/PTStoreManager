cc200ab947575e9dd2d63c15f922aad3d00707d8
dalaryizmi99 <62916535+dalaryizmi99@users.noreply.github.com>
2020-05-05 15:59:53 +0700

    Add project files.

---
+++ PTStoreManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PTStoreManager.Startup))]
+namespace PTStoreManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
